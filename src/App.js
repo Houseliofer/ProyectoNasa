d8bfb60028afd82fcd8881ca2309c9285fa514c5
Houseliofer <20030392@itcelaya.edu.mx>
2023-06-04 23:22:13 -0600

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+//Nasa
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [imageData, setImageData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchImageData();
+  }, []);
+
+  const fetchImageData = async () => {
+    try {
+      const response = await fetch(
+        'https://api.nasa.gov/planetary/apod?api_key=Nmi5B7zcf428OE9Gx2fayYMXalAPSISWYvsmqIQx'
+      );
+      if (!response.ok) {
+        throw new Error('Error fetching image data');
+      }
+      const data = await response.json();
+      setImageData(data);
+      setError(null);
+    } catch (error) {
+      console.error('Error fetching image data:', error);
+      setImageData(null);
+      setError('Error fetching image data');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Imagen Astronómica del Día</h1>
+      {error && <p>{error}</p>}
+      {imageData && (
+        <div>
+          <h2>{imageData.title}</h2>
+          <img src={imageData.url} alt={imageData.title} />
+          <p>{imageData.explanation}</p>
+        </div>
+      )}
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
